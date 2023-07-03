da8fd55158ec18a90c869acb38597c604da9ff4e
sp040739 <SP040739@cerner.net>
2023-07-03 02:33:53 -0400

    x

---
+++ src/main/java/com/example/subha1/Subha1Application.java
@@ -0,0 +1,13 @@
+package com.example.subha1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Subha1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Subha1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/subha1/Subha1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.subha1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Subha1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
